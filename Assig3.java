c0ebdbd17a6d8d552b6ed85705a6f61d70c03a04
misternomar <rlucindo@csumb.edu>
2019-05-19 13:03:27 -0700

    Update Assig3.java

--- Assig3.java
+++ Assig3.java
@@ -455,16 +455,7 @@ class Deck
 	public Deck(int numPacks)
 	{
 	   this.numPacks = numPacks;
-	   Deck.cards = new Card[numPacks * 52];
-	   
-	   char[] cardValDeck = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'};
-      for (int i = 0; i < 4; i++)
-      {
-        for (int j = 0; j < cardValDeck.length; j++)
-        {
-          card[i * cardValDeck.length + j] = new Card(cardValDeck[j], Card.Suit.values()[i]);
-        }
-      }
+	   init(numPacks);
 	   Deck.masterPack = new Card[52];
 	   allocateMasterPack();
 	   haveAllocatedMasterPack = true;
@@ -473,6 +464,7 @@ class Deck
 	public Deck()
 	{
       this.numPacks = 1;
+      init(numPacks);
       Deck.masterPack = new Card[52];
       allocateMasterPack();
       haveAllocatedMasterPack = true;
@@ -498,7 +490,10 @@ class Deck
 	
 	public void init(int numpacks)
 	{
-	   
+	   while (numPacks > 0)
+	   {
+	      
+	   }
 	}
    
 }
